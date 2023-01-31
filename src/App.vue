594dddcb43a00c9675f236c815ec5f00010ac46e
Bao Fei <xiaodou04918@outlook.com>
2023-01-31 16:00:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <router-view />
+  </div>
+</template>
+<script>
+import Nav from "@/views/Nav/index.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+  methods:{
+  },
+
+}
+</script>
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+
+  box-sizing: border-box;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul{
+  list-style: none;
+}
+
+#app {
+  width: 100%;
+  padding: 0 1%;
+
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/views/Nav/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="nav">
+    <strong class="nav_logo">柠檬茶号</strong>
+    <div class="nav_link">
+      <ul>
+        <li>
+          <router-link to="">甲板</router-link>
+        </li>
+        <li>
+          <router-link to="">货舱</router-link>
+        </li>
+        <li>
+          <router-link to="">档案室</router-link>
+        </li>
+        <li>
+          <router-link to="">传达室</router-link>
+        </li>
+        <li>
+          <router-link to="">船长室</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style scoped lang="less">
+#nav {
+  width: 100%;
+  height: 100px;
+
+  border: 4px solid #00a1d6;
+  border-radius: 18px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+
+}
+.nav_logo{
+  font-size: 3rem;
+  font-weight:bold;
+}
+.nav_link{
+  width: 50%;
+
+  font-size: 20px;
+
+  &>ul{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+    align-items: center;
+
+  }
+}
+</style>
